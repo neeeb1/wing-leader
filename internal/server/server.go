30ee97367f42fb86f75a8c05eac8d8688dba8d18
neeeb1 <18238012+neeeb1@users.noreply.github.com>
2026-01-27 07:13:23 -0800

    Add periodic match session cleanup

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,15 +1,20 @@
 package server
 
 import (
+	"context"
 	"net/http"
+	"time"
 
 	"github.com/neeeb1/rate_birds/internal/api"
 	"github.com/rs/zerolog/log"
 )
 
+const cleanUpInterval = 10 * time.Minute
+
 func StartServer(cfg *api.ApiConfig) {
 	mux := http.NewServeMux()
 	api.RegisterEndpoints(mux, cfg)
+	cleanUpTokens(cfg)
 
 	server := http.Server{}
 
@@ -18,4 +23,22 @@ func StartServer(cfg *api.ApiConfig) {
 
 	log.Info().Msgf("now serving at https://localhost:%s", server.Addr)
 	server.ListenAndServe()
+}
+
+func cleanUpTokens(cfg *api.ApiConfig) {
+	log.Info().Msg("Started token cleanup routine")
+	ticker := time.NewTicker(cleanUpInterval)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case t := <-ticker.C:
+				cfg.DbQueries.CleanUpSessions(context.Background())
+				log.Info().Msgf("Cleaned up expired tokens at %v", t)
+			}
+		}
+	}()
 }
